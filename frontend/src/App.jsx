987735e0bac47357ffe02798dce764c82df755d4
unknown <munyaoangelina001@gmail.com>
2026-07-14 13:53:28 +0300

    first commit'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import PropertyList from './components/PropertyList';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Login from './components/Login';
+// import Register from './components/Register';
+// import PropertyList from './components/PropertyList';
+// import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/" component={PropertyList} />
-        </Switch>
+          {/* <Route path="/login" component={Login} /> */}
+          {/* <Route path="/register" component={Register} /> */}
+          {/* <Route path="/" component={PropertyList} /> */}
       </Router>
     </AuthProvider>
   );
